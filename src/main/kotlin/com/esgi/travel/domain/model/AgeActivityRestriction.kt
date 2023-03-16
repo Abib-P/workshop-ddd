0b8a81414de73078e512645d18bd8b90680c3ab7
nathanaelto <nathanael.to@free.fr>
2023-03-16 22:45:20 +0100

    Refacto indent and naming 2

--- src/main/kotlin/com/esgi/travel/domain/model/AgeActivityRestriction.kt
+++ src/main/kotlin/com/esgi/travel/domain/model/AgeActivityRestriction.kt
@@ -1,6 +1,6 @@
 package com.esgi.travel.domain.model
 
-class AgeActiviteRestriction(val minAge: Int, val maxAge: Int) : ActivityRestriction {
+class AgeActivityRestriction(val minAge: Int, val maxAge: Int) : ActivityRestriction {
     init {
         if (minAge > maxAge) {
             throw IllegalArgumentException("minAge must be lower than maxAge")
