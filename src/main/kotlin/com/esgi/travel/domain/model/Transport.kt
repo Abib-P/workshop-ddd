ed621a1656d078055627eb48a307f4cb7bb6cc2a
Chottot <71697340+Chottot@users.noreply.github.com>
2023-03-16 16:26:41 +0100

    Transport constructeur

--- src/main/kotlin/com/esgi/travel/domain/model/Transport.kt
+++ src/main/kotlin/com/esgi/travel/domain/model/Transport.kt
@@ -4,4 +4,15 @@ import java.time.LocalDateTime
 
 class Transport(val type: String, val price: Double, val startTime: LocalDateTime, val arrivedTime: LocalDateTime) {
 
+    init {
+        if (startTime.isAfter(arrivedTime)) {
+            throw IllegalArgumentException("Start time must be before arrived time")
+        }
+        if(price < 0) {
+            throw IllegalArgumentException("Price must be positive")
+        }
+        if(type.isEmpty()) {
+            throw IllegalArgumentException("Type must not be empty")
+        }
+    }
 }
