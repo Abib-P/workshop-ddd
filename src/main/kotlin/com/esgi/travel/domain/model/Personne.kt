c6ae0155e11dfe649df76df3fd64f18ff4c2bf0f
Chottot <71697340+Chottot@users.noreply.github.com>
2023-03-16 16:28:24 +0100

    Personne constructeur

--- src/main/kotlin/com/esgi/travel/domain/model/Personne.kt
+++ src/main/kotlin/com/esgi/travel/domain/model/Personne.kt
@@ -2,6 +2,18 @@ package com.esgi.travel.domain.model
 
 import java.time.LocalDate
 
-class Personne(val mail: Mail, val birthdate: LocalDate, val name: String, val firstName: String, val activites: List<Activite>, val logement: Logement) {
+class Personne(val mail: Mail, val birthdate: LocalDate, val name: String, val firstName: String) {
+
+    init {
+        if (birthdate.isAfter(LocalDate.now())) {
+            throw IllegalArgumentException("La date de naissance ne peut pas être dans le futur")
+        }
+        if (name.isEmpty()) {
+            throw IllegalArgumentException("Le nom ne peut pas être vide")
+        }
+        if (firstName.isEmpty()) {
+            throw IllegalArgumentException("Le prénom ne peut pas être vide")
+        }
+    }
 
 }
