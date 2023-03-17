f5bb5f7a0f41bfe38431413976c940cde6488dd0
Chottot <71697340+Chottot@users.noreply.github.com>
2023-03-17 11:56:49 +0100

    add activity test period collision

--- src/test/kotlin/com/esgi/travel/use_cases/travel/AddActivityToTravelTest.kt
+++ src/test/kotlin/com/esgi/travel/use_cases/travel/AddActivityToTravelTest.kt
@@ -31,65 +31,20 @@ class AddActivityToTravelTest {
     }
 
     @Test
-    fun `should add activity to travel` () {
+    fun `should add activity to travel`() {
 
-        whenever(activities.getById(any())).thenReturn(Activity(
-            id = GenericID.EMPTY,
-            name = "Activity name",
-            description = "Activity description",
-            price = 10.0,
-            duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
-            restrictions = listOf(),
-            address = Address("City", "Country"),
-            capacity = 10
-        ))
-
-        whenever(travels.getById(any())).thenReturn(
-            Travel(
+        whenever(activities.getById(any())).thenReturn(
+            Activity(
                 id = GenericID.EMPTY,
-                period = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
-                  client = Client(
-                        GenericID.EMPTY,
-                        Mail("mail@mail"),
-                        LocalDate.now().minusYears(18),
-                        "patate",
-                        "patate"),
-                destination = Address("City", "Country"),
-                attendants = listOf(
-                    Attendant(
-                        GenericID("1"),
-                        Mail("mail@mail"),
-                        LocalDate.now().minusYears(18),
-                        "patate",
-                        "patate"),
-                    Attendant(
-                        GenericID("2"),
-                        Mail("mail2@mail"),
-                        LocalDate.now().minusYears(18),
-                        "patate2",
-                        "patate2")
-                ),
-                budget = Budget(0.0, 100.0),
-                state = TravelState.OPENED,
-                activitiesIds = mapOf()
-        ))
-
-        assertDoesNotThrow { addActivity.addActivityToTravel(AddActivityToTravelRequest(GenericID.EMPTY, GenericID.EMPTY, listOf(GenericID("1"), GenericID("2")))) }
-    }
-
-    @Test
-    fun `should not add activity to travel when attendant does not satisfied activity age restriction` () {
-
-        whenever(activities.getById(any())).thenReturn(Activity(
-            id = GenericID.EMPTY,
-            name = "Activity name",
-            description = "Activity description",
-            price = 10.0,
-            duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
-            restrictions = listOf(AgeActivityRestriction(18, 100)),
-            address = Address("City", "Country"),
-            capacity = 10
-        ))
+                name = "Activity name",
+                description = "Activity description",
+                price = 10.0,
+                duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
+                restrictions = listOf(),
+                address = Address("City", "Country"),
+                capacity = 10
+            )
+        )
 
         whenever(travels.getById(any())).thenReturn(
             Travel(
@@ -100,42 +55,57 @@ class AddActivityToTravelTest {
                     Mail("mail@mail"),
                     LocalDate.now().minusYears(18),
                     "patate",
-                    "patate"),
+                    "patate"
+                ),
                 destination = Address("City", "Country"),
                 attendants = listOf(
                     Attendant(
                         GenericID("1"),
                         Mail("mail@mail"),
-                        LocalDate.now().minusYears(17),
+                        LocalDate.now().minusYears(18),
                         "patate",
-                        "patate"),
+                        "patate"
+                    ),
                     Attendant(
                         GenericID("2"),
                         Mail("mail2@mail"),
                         LocalDate.now().minusYears(18),
                         "patate2",
-                        "patate2")
+                        "patate2"
+                    )
                 ),
                 budget = Budget(0.0, 100.0),
                 state = TravelState.OPENED,
                 activitiesIds = mapOf()
-            ))
+            )
+        )
 
-        assertThrows<Exception> { addActivity.addActivityToTravel(AddActivityToTravelRequest(GenericID.EMPTY, GenericID.EMPTY, listOf(GenericID("1"), GenericID("2")))) }
+        assertDoesNotThrow {
+            addActivity.addActivityToTravel(
+                AddActivityToTravelRequest(
+                    GenericID.EMPTY,
+                    GenericID.EMPTY,
+                    listOf(GenericID("1"), GenericID("2"))
+                )
+            )
+        }
     }
 
     @Test
-    fun `should not add activity to travel when activity is full` () {
-        whenever(activities.getById(any())).thenReturn(Activity(
-            id = GenericID.EMPTY,
-            name = "Activity name",
-            description = "Activity description",
-            price = 10.0,
-            duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
-            restrictions = listOf(AgeActivityRestriction(18, 100)),
-            address = Address("City", "Country"),
-            capacity = 1
-        ))
+    fun `should not add activity to travel when attendant does not satisfied activity age restriction`() {
+
+        whenever(activities.getById(any())).thenReturn(
+            Activity(
+                id = GenericID.EMPTY,
+                name = "Activity name",
+                description = "Activity description",
+                price = 10.0,
+                duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
+                restrictions = listOf(AgeActivityRestriction(18, 100)),
+                address = Address("City", "Country"),
+                capacity = 10
+            )
+        )
 
         whenever(travels.getById(any())).thenReturn(
             Travel(
@@ -146,7 +116,8 @@ class AddActivityToTravelTest {
                     Mail("mail@mail"),
                     LocalDate.now().minusYears(18),
                     "patate",
-                    "patate"),
+                    "patate"
+                ),
                 destination = Address("City", "Country"),
                 attendants = listOf(
                     Attendant(
@@ -154,34 +125,47 @@ class AddActivityToTravelTest {
                         Mail("mail@mail"),
                         LocalDate.now().minusYears(17),
                         "patate",
-                        "patate"),
+                        "patate"
+                    ),
                     Attendant(
                         GenericID("2"),
                         Mail("mail2@mail"),
                         LocalDate.now().minusYears(18),
                         "patate2",
-                        "patate2")
+                        "patate2"
+                    )
                 ),
                 budget = Budget(0.0, 100.0),
                 state = TravelState.OPENED,
                 activitiesIds = mapOf()
-            ))
+            )
+        )
 
-        assertThrows<Exception> { addActivity.addActivityToTravel(AddActivityToTravelRequest(GenericID.EMPTY, GenericID.EMPTY, listOf(GenericID("1"), GenericID("2")))) }
+        assertThrows<Exception> {
+            addActivity.addActivityToTravel(
+                AddActivityToTravelRequest(
+                    GenericID.EMPTY,
+                    GenericID.EMPTY,
+                    listOf(GenericID("1"), GenericID("2"))
+                )
+            )
+        }
     }
 
     @Test
-    fun `should not add activity to travel when activity does not exist` () {
-        whenever(activities.getById(any())).thenReturn(Activity(
-            id = GenericID.EMPTY,
-            name = "Activity name",
-            description = "Activity description",
-            price = 10.0,
-            duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
-            restrictions = listOf(AgeActivityRestriction(18, 100)),
-            address = Address("City", "Country"),
-            capacity = 1
-        ))
+    fun `should not add activity to travel when activity is full`() {
+        whenever(activities.getById(any())).thenReturn(
+            Activity(
+                id = GenericID.EMPTY,
+                name = "Activity name",
+                description = "Activity description",
+                price = 10.0,
+                duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
+                restrictions = listOf(AgeActivityRestriction(18, 100)),
+                address = Address("City", "Country"),
+                capacity = 1
+            )
+        )
 
         whenever(travels.getById(any())).thenReturn(
             Travel(
@@ -192,7 +176,8 @@ class AddActivityToTravelTest {
                     Mail("mail@mail"),
                     LocalDate.now().minusYears(18),
                     "patate",
-                    "patate"),
+                    "patate"
+                ),
                 destination = Address("City", "Country"),
                 attendants = listOf(
                     Attendant(
@@ -200,52 +185,135 @@ class AddActivityToTravelTest {
                         Mail("mail@mail"),
                         LocalDate.now().minusYears(17),
                         "patate",
-                        "patate"),
+                        "patate"
+                    ),
                     Attendant(
                         GenericID("2"),
                         Mail("mail2@mail"),
                         LocalDate.now().minusYears(18),
                         "patate2",
-                        "patate2")
+                        "patate2"
+                    )
                 ),
                 budget = Budget(0.0, 100.0),
                 state = TravelState.OPENED,
                 activitiesIds = mapOf()
-            ))
+            )
+        )
 
-        assertThrows<Exception> { addActivity.addActivityToTravel(AddActivityToTravelRequest(GenericID.EMPTY, GenericID.EMPTY, listOf(GenericID("1"), GenericID("2")))) }
+        assertThrows<Exception> {
+            addActivity.addActivityToTravel(
+                AddActivityToTravelRequest(
+                    GenericID.EMPTY,
+                    GenericID.EMPTY,
+                    listOf(GenericID("1"), GenericID("2"))
+                )
+            )
+        }
     }
 
     @Test
-    fun `should not add activity to travel when travel does not exist` () {
-        whenever(activities.getById(any())).thenReturn(Activity(
-            id = GenericID.EMPTY,
-            name = "Activity name",
-            description = "Activity description",
-            price = 10.0,
-            duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
-            restrictions = listOf(AgeActivityRestriction(18, 100)),
-            address = Address("City", "Country"),
-            capacity = 1
-        ))
+    fun `should not add activity to travel when activity does not exist`() {
+        whenever(activities.getById(any())).thenReturn(
+            Activity(
+                id = GenericID.EMPTY,
+                name = "Activity name",
+                description = "Activity description",
+                price = 10.0,
+                duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
+                restrictions = listOf(AgeActivityRestriction(18, 100)),
+                address = Address("City", "Country"),
+                capacity = 1
+            )
+        )
+
+        whenever(travels.getById(any())).thenReturn(
+            Travel(
+                id = GenericID.EMPTY,
+                period = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
+                client = Client(
+                    GenericID.EMPTY,
+                    Mail("mail@mail"),
+                    LocalDate.now().minusYears(18),
+                    "patate",
+                    "patate"
+                ),
+                destination = Address("City", "Country"),
+                attendants = listOf(
+                    Attendant(
+                        GenericID("1"),
+                        Mail("mail@mail"),
+                        LocalDate.now().minusYears(17),
+                        "patate",
+                        "patate"
+                    ),
+                    Attendant(
+                        GenericID("2"),
+                        Mail("mail2@mail"),
+                        LocalDate.now().minusYears(18),
+                        "patate2",
+                        "patate2"
+                    )
+                ),
+                budget = Budget(0.0, 100.0),
+                state = TravelState.OPENED,
+                activitiesIds = mapOf()
+            )
+        )
+
+        assertThrows<Exception> {
+            addActivity.addActivityToTravel(
+                AddActivityToTravelRequest(
+                    GenericID.EMPTY,
+                    GenericID.EMPTY,
+                    listOf(GenericID("1"), GenericID("2"))
+                )
+            )
+        }
+    }
+
+    @Test
+    fun `should not add activity to travel when travel does not exist`() {
+        whenever(activities.getById(any())).thenReturn(
+            Activity(
+                id = GenericID.EMPTY,
+                name = "Activity name",
+                description = "Activity description",
+                price = 10.0,
+                duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
+                restrictions = listOf(AgeActivityRestriction(18, 100)),
+                address = Address("City", "Country"),
+                capacity = 1
+            )
+        )
 
         whenever(travels.getById(any())).thenReturn(null)
 
-        assertThrows<Exception> { addActivity.addActivityToTravel(AddActivityToTravelRequest(GenericID.EMPTY, GenericID.EMPTY, listOf(GenericID("1"), GenericID("2")))) }
+        assertThrows<Exception> {
+            addActivity.addActivityToTravel(
+                AddActivityToTravelRequest(
+                    GenericID.EMPTY,
+                    GenericID.EMPTY,
+                    listOf(GenericID("1"), GenericID("2"))
+                )
+            )
+        }
     }
 
     @Test
-    fun `should not add activity to travel when attendant does not exist` () {
-        whenever(activities.getById(any())).thenReturn(Activity(
-            id = GenericID.EMPTY,
-            name = "Activity name",
-            description = "Activity description",
-            price = 10.0,
-            duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
-            restrictions = listOf(),
-            address = Address("City", "Country"),
-            capacity = 10
-        ))
+    fun `should not add activity to travel when attendant does not exist`() {
+        whenever(activities.getById(any())).thenReturn(
+            Activity(
+                id = GenericID.EMPTY,
+                name = "Activity name",
+                description = "Activity description",
+                price = 10.0,
+                duration = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 1, 0, 10)),
+                restrictions = listOf(),
+                address = Address("City", "Country"),
+                capacity = 10
+            )
+        )
 
         whenever(travels.getById(any())).thenReturn(
             Travel(
@@ -256,7 +324,8 @@ class AddActivityToTravelTest {
                     Mail("mail@mail"),
                     LocalDate.now().minusYears(18),
                     "patate",
-                    "patate"),
+                    "patate"
+                ),
                 destination = Address("City", "Country"),
                 attendants = listOf(
                     Attendant(
@@ -264,14 +333,94 @@ class AddActivityToTravelTest {
                         Mail("mail@mail"),
                         LocalDate.now().minusYears(17),
                         "patate",
-                        "patate")
+                        "patate"
+                    )
                 ),
                 budget = Budget(0.0, 100.0),
                 state = TravelState.OPENED,
                 activitiesIds = mapOf()
-            ))
+            )
+        )
 
-        assertThrows<Exception> { addActivity.addActivityToTravel(AddActivityToTravelRequest(GenericID.EMPTY, GenericID.EMPTY, listOf(GenericID("1"), GenericID("2")))) }
+        assertThrows<Exception> {
+            addActivity.addActivityToTravel(
+                AddActivityToTravelRequest(
+                    GenericID.EMPTY,
+                    GenericID.EMPTY,
+                    listOf(GenericID("1"), GenericID("2"))
+                )
+            )
+        }
+    }
+
+    @Test
+    fun `should not add activity to travel when attendant already have a activity at this time`() {
+        whenever(activities.getById(activityId = GenericID("activity1"))).thenReturn(
+            Activity(
+                id = GenericID("activity1"),
+                name = "Activity name",
+                description = "Activity description",
+                price = 10.0,
+                duration = Period(LocalDateTime.of(2020, 1, 1, 8, 0), LocalDateTime.of(2020, 1, 1, 10, 10)),
+                restrictions = listOf(),
+                address = Address("City", "Country"),
+                capacity = 10
+            )
+        )
+
+        whenever(activities.getById(activityId = GenericID("activity2"))).thenReturn(
+            Activity(
+                id = GenericID("activity2"),
+                name = "Activity name",
+                description = "Activity description",
+                price = 10.0,
+                duration = Period(LocalDateTime.of(2020, 1, 1, 10, 11), LocalDateTime.of(2020, 1, 1, 10, 30)),
+                restrictions = listOf(),
+                address = Address("City", "Country"),
+                capacity = 10
+            )
+        )
+
+        whenever(travels.getById(any())).thenReturn(
+            Travel(
+                id = GenericID.EMPTY,
+                period = Period(LocalDateTime.of(2020, 1, 1, 0, 0), LocalDateTime.of(2020, 1, 10, 0, 10)),
+                client = Client(
+                    GenericID.EMPTY,
+                    Mail("mail@mail"),
+                    LocalDate.now().minusYears(18),
+                    "patate",
+                    "patate"
+                ),
+                destination = Address("City", "Country"),
+                attendants = listOf(
+                    Attendant(
+                        GenericID("attendant1"),
+                        Mail("mail@mail"),
+                        LocalDate.now().minusYears(20),
+                        "patate",
+                        "patate"
+                    )
+                ),
+                budget = Budget(0.0, 100.0),
+                state = TravelState.OPENED,
+                activitiesIds = mapOf(
+                    GenericID("activity1") to listOf(
+                        GenericID("attendant1")
+                    )
+                )
+            )
+        )
+
+        assertThrows<Exception> {
+            addActivity.addActivityToTravel(
+                AddActivityToTravelRequest(
+                    GenericID.EMPTY,
+                    GenericID("activity2"),
+                    listOf(GenericID("attendant1"))
+                )
+            )
+        }
     }
 
 }
